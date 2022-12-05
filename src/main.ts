c51b966f6b90b828a4efba3c67876fc4d9a41b46
Pat <pat@tilde.team>
2022-12-04 21:40:41 -0500

    use javascript to submit form

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,31 @@
 import './style.css'
 
-console.info("Hello, world!")
+function getFormData(): void {
+  const marketingPreferenceForm = document.getElementById("marketing-preference-form") as HTMLFormElement;
+  console.info({ marketingPreferenceForm });
+
+  const email = document.getElementById("email") as HTMLInputElement;
+  console.info({ email: email.value });
+
+  const interests = document.getElementById("interest-select") as HTMLSelectElement;
+  console.info({ interests: interests.selectedOptions });
+
+  const frequency = document.getElementById("frequency-select") as HTMLSelectElement;
+  console.info({ frequency: frequency.selectedOptions });
+
+  // Build formData object.
+  let formData = new FormData();
+  formData.append('email', email?.value);
+  formData.append('password', 'John123');
+
+  fetch("api/marketing-preference",
+    {
+      body: formData,
+      method: "post"
+    });
+}
+
+const submitButton = document.getElementById("marketing-preference-submit") as HTMLButtonElement;
+if (submitButton) {
+  submitButton.addEventListener("click", getFormData);
+}
