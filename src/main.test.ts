3ff42dc63aa9e0700f4de2a61a8994fdcad3882e
Pat <pat@tilde.team>
2022-12-10 22:36:05 -0500

    add timestamp

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,23 +1,17 @@
-import { assert, describe, expect, it } from 'vitest'
-import { addToNumbers } from './main'
+import { describe, expect, it } from 'vitest'
+import { addToNumbers, extractOptions } from './main'
 
-describe('suite name', () => {
-  it('foo', () => {
-    expect(1 + 1).toEqual(2)
-    expect(true).to.be.true
-  })
 
-  it('bar', () => {
-    assert.equal(Math.sqrt(4), 2)
-  })
-
-  it('snapshot', () => {
-    expect({ foo: 'bar' }).toMatchSnapshot()
+describe('add two numbers should return correct result', () => {
+  it('should return three when you add one and two', () => {
+    expect(addToNumbers(1, 2) === 3).to.be.true
   })
 })
 
-describe('add two numbers should return correct result', () => {
-    it('should return three when you add one and two', () => {
-        expect(addToNumbers(1, 2) === 3).to.be.true
-    })
+describe('extractOptions should return correct string', () => {
+  it('should return empty string for an empty collection', () => {
+    const interests = document.getElementById("interest-select") as HTMLSelectElement;
+    let interestsHtmlCollection = interests.selectedOptions;
+    expect(extractOptions(interestsHtmlCollection), "should return empty string").toEqual("");
+  })
 })
