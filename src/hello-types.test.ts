754e84087d6638d199a0f410ca2f01a2babd8d75
Pat <pat@tilde.team>
2022-12-10 21:46:52 -0500

    add timestamp

--- src/hello-types.test.ts
+++ src/hello-types.test.ts
@@ -1,23 +1,8 @@
-import { assert, describe, expect, it } from 'vitest'
+import { assert, describe, it } from 'vitest'
 import addTwoNumbers from './hello-types'
-
-describe('suite name', () => {
-  it('foo', () => {
-    expect(1 + 1).toEqual(2)
-    expect(true).to.be.true
-  })
-
-  it('bar', () => {
-    assert.equal(Math.sqrt(4), 2)
-  })
-
-  it('snapshot', () => {
-    expect({ foo: 'bar' }).toMatchSnapshot()
-  })
-})
 
 describe('add two numbers should return correct result', () => {
     it('should return three when you add one and two', () => {
-        assert.equal(addTwoNumbers(1, 2) === 3)
+        assert.equal(addTwoNumbers(1, 2), 3)
     })
 })
